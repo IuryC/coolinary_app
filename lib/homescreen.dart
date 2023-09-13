19d61d362c8a5a80d6d7f69599a9595db7d88510
IuryC <iurycauan0352@hotmail.com>
2023-09-13 19:44:41 -0300

    ok

---
+++ lib/homescreen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class RecipeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Receitas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            RecipeCard(
+              title: 'Bolo de Chocolate',
+              description: 'Uma deliciosa receita de bolo de chocolate.',
+              imageUrl:
+              'https://example.com/images/chocolate-cake.jpg', // Substitua pela URL da imagem da receita
+            ),
+            RecipeCard(
+              title: 'Sopa de Legumes',
+              description: 'Uma sopa de legumes saudável e reconfortante.',
+              imageUrl:
+              'https://example.com/images/vegetable-soup.jpg', // Substitua pela URL da imagem da receita
+            ),
+            // Adicione mais RecipeCards conforme necessário
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  RecipeCard({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      margin: EdgeInsets.only(bottom: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.network(
+            imageUrl,
+            width: double.infinity,
+            height: 200.0,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  description,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(RecipeApp());
+}
